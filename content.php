df86c905aa704639acb065435c7ed6f8126c2f85
Elsa Brown <elsambrown@gmail.com>
2018-01-23 15:19:01 -0500

    initial commit

---
+++ content.php
@@ -0,0 +1,80 @@
+	<article id="landing">
+		<div>
+			<h1>COOP FUND</h1>
+			<div id="bucket"></div>
+		</div>
+		<div id="stats">
+			<div class="stat col-md-4">
+				<div class="stat-header">
+					<h3>Total Members</h3>
+				</div>
+				<p class="number">3</p>
+			</div>
+			<div class="stat col-md-4">
+				<div class="stat-header">
+					<h3>Fund<br/>Balance</h3>
+				</div>
+				<p class="number">$0</p>
+			</div>
+			<div class="stat col-md-4">
+				<div class="stat-header">
+					<h3>Projects Funded</h3>
+				</div>
+				<p class="number">0</p>
+			</div>
+		</article>
+		<!-- <a name="mission-statement" class="adjust"></a> -->
+		<div class="left-aligned">
+			<p class="tab"></p><p>Coop Fund is an experimental cooperative funding platform that accumulates financial resources through member contributions and redistributes these funds based on a cooperative decision making process. Read on to learn more!</p>
+		</div>
+	</div>
+	<a name="about" class="adjust"></a>
+	<article>
+		<p><span class="bold">About</span><br /><br />
+		This project is in resistance to accepting the current limited forms of investor owned, corporate, for profit business models that dominate most areas of life, and it brings into question the non-profit model and it’s over reliance on wealthy elites.<br /><br />
+		We recognise that we are working in the field of cultural production, but we don’t see this field as exceptional or exempt from the need to develop ideas for structural change when it comes to wealth distribution and democratic work practices. In fact we know that this field is highly unregulated, exploitative and exclusionary at all levels.<br /><br />
+	<span class="bold">History and Current Format</span><br /><br />
+		In the beginning of 2017, as part of <a href="http://artistsspace.org/exhibitions/authorization-sessions">Authorization Sessions</a> at Artists Space, Emma Hedditch organized an eight week Cooperative Study Discussion Course with Artists Space staff members Rachel Peddersen, Harry Burke, Colleen O’Connor and Jamie Stevens, and were joined for some of the sessions by Greer Dworman, Tess Dworman, Michelle Coffey, Richard Birkett, and Laurel Atwell.<br /><br />
+		Following this course and further research into cooperative organization in Edinburgh, with Rhubaba Gallery and The Showroom in London. In August 2017 Emma joined with Lydia Okrent and Elsa Brown in NY and together they started to work on the administration of an online and in person cooperative funding platform, Coop Fund.
+		</p>
+	</article>
+	<article>
+		<p><span class="bold">Authorization Sessions 2018</span><br /><br />Throughout the course of the Authorization Sessions exhibition, there will be bi-weekly meetings with the expanded membership of the Coop Fund and interested parties, to further develop the Fund and its constitution. Participants in these meetings will be paid a fee. At the end of the exhibition period we will host two meetings/workshops with other coops and coops-to-be on how to start their own cooperative fund.
+		<a name="what-is-a-coop" class="adjust-large"></a>
+		</p>
+		<br />
+		<br />
+		<p><span class="bold">What Is A Cooperative?</span><br /><br />
+		"A cooperative is an autonomous association of persons united voluntarily to meet their common economic, social, and cultural needs and aspirations through a jointly-owned and democratically-controlled enterprise."<br /><span class="small">- International Cooperative Alliance Statement on Cooperative Identity</span><br /><br />
+		Cooperatives are based on the values of
+		<ul>
+			<li>self-help,</li> 
+			<li>self-responsibility,</li>
+			<li>democracy,</li>
+			<li>equality,</li>
+			<li>equity,</li>
+			<li>and solidarity.</li>
+		</ul>
+		In the tradition of their founders, cooperative members believe in the ethical values of honesty, openness, social responsibility, and caring for others.</p>
+	</article>
+	<a name="how-it-works" class="adjust-large"></a>
+	<article>
+		<span class="bold">How It Works</span><br /><br />
+		<ol class="sans">
+			<div class="image-small" id="book"></div>
+			<li>Read the Coop Fund Purpose, Principles, Participation documents</li>
+			<li>Decide if you want to become a member</li>
+			<li>Become a member (link to sign up page)</li>
+			<li>Subscribe w/ a minimum of $2 a month (you can increase your subscription if and when you have the means to do so).</li>
+			<li>As a member you have access to the discussions and decision making forum on the site Loomio</li>
+			<li>Propose projects that you want to have funded</li>
+			<li>Vote on other members projects</li>
+			<li>Funds are distributed to approved proposals</li>
+		</ol>
+	</article>
+	<a name="definitions" class="adjust-large"></a>
+	<article>
+		<span class="bold">Definitions</span><br /><br />
+	</article>
+	</main>
+</div>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Coop Fund</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/style.css" />
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div id="body-wrapper">
+		<nav>
+			<div id="nav-left">
+				<a href="#home"><p class="title">COOP FUND</p></a>
+				<!-- <p><a href="#mission-statement">Mission Statement</a></p> -->
+				<p><a href="#about">About</a></p>
+				<a href="#what-is-a-coop"><p>What Is A Cooperative?</p></a>
+				<a href="#how-it-works"><p>How It Works</p></a>
+				<a href="#definitions"><p>Definitions and Appendix</p></a>
+			</div>
+			<div id="nav-right">
+				<p>Sign Up / Log In</p>
+			</div>
+		</nav>
+		<a name="home" class="adjust"></a>
+		<main>
